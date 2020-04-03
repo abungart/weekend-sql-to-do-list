4a7239f185e2153bd9b61b4ad9f6fa54d3c9b596
Adam Bungart <abungart@gmail.com>
2020-04-03 16:24:01 -0500

    Added Post route

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -18,4 +18,26 @@ router.get("/", (req, res) => {
     });
 });
 
+// POST ROUTE
+router.post("/", (req, res) => {
+  const taskFromClient = req.body;
+  const queryText = `INSERT INTO "todo" ("username", "task", "task_completed")
+    VALUES ($1, $2, $3);`;
+
+  pool
+    .query(queryText, [
+      taskFromClient.name,
+      taskFromClient.task,
+      taskFromClient.completed,
+    ])
+    .then((responseDb) => {
+      console.log(responseDb);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log("ERROR:", err);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
